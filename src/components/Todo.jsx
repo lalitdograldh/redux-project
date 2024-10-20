97f23e8ce3b7267fd6e1506b1d8612cf0e3f2ace
lalitdograldh <lalitdogra04@gmail.com>
2024-10-20 21:40:50 +0530

    showing data in list

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,34 @@
-export const Todo = () =>{
-    return (
-        <div className="container">
-            <div className="todo-app">
-                <h1>
-                    <i className="fa-regular fa-pen-to-sqaure"></i>To-do List:
-                </h1>
-                <div className="row">
-                    <form>
-                        <input type="text" id="input-box" placeholder="Add a new task"/>
-                        <button>Add Task</button>
-                    </form>
-                </div>
-                <ul id="list-container"></ul>
-            </div>
+import { useSelector } from "react-redux";
+import { MdDeleteForever} from "react-icons/md";
+export const Todo = () => {
+  const tasks = useSelector((state) => state.task);
+  return (
+    <div className="container">
+      <div className="todo-app">
+        <h1>
+          <i className="fa-regular fa-pen-to-sqaure"></i>To-do List:
+        </h1>
+        <div className="row">
+          <form>
+            <input type="text" id="input-box" placeholder="Add a new task" />
+            <button>Add Task</button>
+          </form>
         </div>
-      );
-}
+        <ul id="list-container">
+          {tasks.map((curTask, index) => {
+            return (
+              <li key={index}>
+                <p>
+                  {index} : {curTask}
+                </p>
+                <div>
+                    <MdDeleteForever className="icon-style" onClick={() =>handleTaskDelete(index)}></MdDeleteForever>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
+};
